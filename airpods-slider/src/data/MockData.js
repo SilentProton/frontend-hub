6dc50caf642044b791fd73f5408618e763e6340e
SilentProton <silentproton.25@gmail.com>
2025-09-05 23:04:23 +0530

    chore: add mock data for development

---
+++ airpods-slider/src/data/MockData.js
@@ -0,0 +1,56 @@
+import Img1 from "../assets/1.png"
+import Img2 from "../assets/2.png"
+import Img3 from "../assets/3.png"
+
+export const NavbarData = [
+    {
+        id: 1,
+        title: "Home",
+        link: "/",
+    },
+    {
+        id: 2,
+        title: "Categories",
+        link: "#",
+    },
+    {
+        id: 3,
+        title: "Blog",
+        link: "#",
+    },
+    {
+        id: 4,
+        title: "Contact",
+        link: "#",
+    },
+]
+
+export const HeadphoneData = [
+    {
+        id: 1,
+        title: "Apple AirPods Wireless Over-Ear Headphones",
+        subtitle: 
+            "Experience next-level sound with Apple AirPods Wireless Over-Ear Headphones, designed to deliver immersive spatial audio, powerful noise cancellation, and all-day comfort. With seamless connectivity across your Apple devices and premium build quality, they bring clarity, style, and performance together in one perfect fit.",
+        price: "$40.99",
+        model: "AirPods",
+        bgColor: "#e85950",
+    },
+    {
+        id: 2,
+        title: "Apple AirPods Wireless Over-Ear Headphones",
+        subtitle: 
+            "Experience next-level sound with Apple AirPods Wireless Over-Ear Headphones, designed to deliver immersive spatial audio, powerful noise cancellation, and all-day comfort. With seamless connectivity across your Apple devices and premium build quality, they bring clarity, style, and performance together in one perfect fit.",
+        price: "$40.99",
+        model: "AirPods",
+        bgColor: "#24b4bc",
+    },
+    {
+        id: 1,
+        title: "Apple AirPods Wireless Over-Ear Headphones",
+        subtitle: 
+            "Experience next-level sound with Apple AirPods Wireless Over-Ear Headphones, designed to deliver immersive spatial audio, powerful noise cancellation, and all-day comfort. With seamless connectivity across your Apple devices and premium build quality, they bring clarity, style, and performance together in one perfect fit.",
+        price: "$40.99",
+        model: "AirPods",
+        bgColor: "#7aa10e",
+    },
+]
