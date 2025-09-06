c365f2f388a09b51a6c63077710e2cf035636f0e
SilentProton <silentproton.25@gmail.com>
2025-09-06 22:40:25 +0530

    feat: add navbar component

---
+++ airpods-slider/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Hero = () => {
+  return (
+    <section className='bg-red-400'>
+        <Navbar/>
+        <div className="container grid grid-cols-1 md:grid-cols-2 h-screen md:h-[700px] relative">
+        </div>
+    </section>
+  )
+}
+
+export default Hero
