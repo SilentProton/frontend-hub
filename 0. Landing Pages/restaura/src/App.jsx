57e3b4d76ecb36a3475b47b68c25f3b07668a40c
SilentProton <silentproton.25@gmail.com>
2025-09-13 20:00:35 +0530

    feat: add hero section

--- 0. Landing Pages/restaura/src/App.jsx
+++ 0. Landing Pages/restaura/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline bg-white">    Hello world!  </h1>
+      <HeroSection/>
     </div>
   )
 }
