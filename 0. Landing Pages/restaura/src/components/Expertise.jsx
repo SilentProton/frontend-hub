9d9847376442803715c9e5bafa5169a5ef848b07
SilentProton <silentproton.25@gmail.com>
2025-09-14 16:02:12 +0530

    feat: add motion for animations

--- 0. Landing Pages/restaura/src/components/Expertise.jsx
+++ 0. Landing Pages/restaura/src/components/Expertise.jsx
@@ -1,12 +1,41 @@
 import { CUSINES } from "../constants"
+import { motion } from "motion/react"
+
+const containerVariants = {
+  hidden: {opacity: 0},
+  show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 1,
+      }
+  }
+}
+
+const itemVariants = {
+  hidden: {opacity: 0, y: 20},
+  show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+      }
+  }
+}
 
 const Expertise = () => {
   return (
     <section id="expertise">
         <h2 className="my-8 text-center text-3xl tracking-tighter">Our Expertise</h2>
-        <div className="container mx-auto px-4">
+        <motion.div 
+            initial="hidden"
+            whileInView="show"
+            variants={containerVariants}
+            className="container mx-auto px-4">
             {CUSINES.map((cusine, index) => (
-                <div key={index} className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2">
+                <motion.div 
+                    key={index} 
+                    variants={itemVariants}
+                    className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2">
                     <div className="flex-shrink-0 pr-8 text-2xl">{cusine.number}</div>
                     <div className="w-1/3 flex-shrink-0">
                       <img 
@@ -22,9 +51,9 @@ const Expertise = () => {
                         {cusine.description}
                       </p>
                     </div>
-                </div>
+                </motion.div>
             ))}
-        </div>
+        </motion.div>
     </section>
   )
 }
--- 0. Landing Pages/restaura/src/components/HeroSection.jsx
+++ 0. Landing Pages/restaura/src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import video from "../assets/hero.mp4"
 import logo from "../assets/logo.png"
 import hero from "../assets/hero.jpeg"
+import { motion } from "motion/react"
 
 const HeroSection = () => {
   return (
@@ -11,7 +11,13 @@ const HeroSection = () => {
         </div>
         <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black"></div>
         <div className="relative z-10 flex h-screen flex-col justify-end pb-20">
-            <img src={logo} alt="reataura" className='w-full p-4'/>
+            <motion.img 
+              initial={{ opacity: 0, y:50 }}
+              animate={{ opacity: 1, y:0 }}
+              transition={{duration: 0.5}}
+              src={logo} 
+              alt="reataura" 
+              className='w-full p-4'/>
             <p className='p-4 text-lg tracking-tight text-white'>India</p>
         </div>
     </section>
